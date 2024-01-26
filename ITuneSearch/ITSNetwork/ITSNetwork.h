65d6ea0dabcc14e21399e8824c0bc5b821c8ecac
oguzhanyalcin <oyalcin@innova.com.tr>
2024-01-27 01:27:28 +0300

    ITSNetwork framework is added.

---
+++ ITuneSearch/ITSNetwork/ITSNetwork.h
@@ -0,0 +1,18 @@
+//
+//  ITSNetwork.h
+//  ITSNetwork
+//
+//  Created by Oguzhan Yalcin on 27.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ITSNetwork.
+FOUNDATION_EXPORT double ITSNetworkVersionNumber;
+
+//! Project version string for ITSNetwork.
+FOUNDATION_EXPORT const unsigned char ITSNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ITSNetwork/PublicHeader.h>
+
+
